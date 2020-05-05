a8f5db3bf735460571f538dd2e4e5206be45e238
kai <921485453@qq.com>
2020-05-05 19:59:33 +0800

    用户头像保存修正

--- src/main/java/njgis/opengms/portal/service/UserService.java
+++ src/main/java/njgis/opengms/portal/service/UserService.java
@@ -902,7 +902,7 @@ public class UserService {
         BeanUtils.copyProperties(userUpdateDTO,user);
         //判断是否为新图片
         String uploadImage=userUpdateDTO.getUploadImage();
-        if(!uploadImage.contains("/user/")) {
+        if(!uploadImage.contains("base64")) {
             //删除旧图片
             File file=new File(resourcePath+user.getImage());
             if(file.exists()&&file.isFile())
