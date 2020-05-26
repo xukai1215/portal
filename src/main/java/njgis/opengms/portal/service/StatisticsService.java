06e31524efe2aa116a2fb55e53836abdc80d4519
kai <921485453@qq.com>
2020-05-26 22:52:11 +0800

    theme 样式修改

--- src/main/java/njgis/opengms/portal/service/StatisticsService.java
+++ src/main/java/njgis/opengms/portal/service/StatisticsService.java
@@ -2,14 +2,20 @@ package njgis.opengms.portal.service;
 
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import njgis.opengms.portal.dao.ComputableModelDao;
 import njgis.opengms.portal.dao.UserDao;
+import njgis.opengms.portal.entity.ComputableModel;
+import njgis.opengms.portal.entity.Item;
 import njgis.opengms.portal.entity.User;
 import njgis.opengms.portal.entity.ViewRecord;
+import njgis.opengms.portal.entity.support.DailyViewCount;
 import njgis.opengms.portal.entity.support.GeoInfoMeta;
 import njgis.opengms.portal.utils.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -18,6 +24,153 @@ public class StatisticsService {
 
     @Autowired
     UserDao userDao;
+
+    @Autowired
+    ComputableModelDao computableModelDao;
+
+    SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
+
+    public void getComputableModelStatisticsInfo(String oid) {
+        JSONObject result = new JSONObject();
+        ComputableModel computableModel=computableModelDao.findFirstByOid(oid);
+        result.put("invokeCount", computableModel.getInvokeCount());
+        result.put("viewCount", computableModel.getViewCount());
+
+    }
+
+    public JSONObject getDailyViewAndInvokeTimes(Item item, List<ComputableModel> computableModelList){
+        List<DailyViewCount> dailyViewCountList=item.getDailyViewCount();
+        JSONArray dateList = new JSONArray();
+        dateList.add("Timeline");
+        JSONArray viewArray=new JSONArray();
+        viewArray.add("View Times");
+        JSONArray invokeArray = new JSONArray();
+        invokeArray.add("Invoke Times");
+        JSONArray resultList = new JSONArray();
+        Date now = new Date();
+
+        Calendar c = Calendar.getInstance();//动态时间
+        c.setTime(now);
+        String startTime;//chart起始时间
+        int max=0;
+        if(dailyViewCountList==null||dailyViewCountList.size()==0){
+
+
+            c.add(Calendar.DATE,-6);
+            startTime=sdf.format(c.getTime());
+            for(int i=0;i<6;i++){
+                dateList.add(sdf.format(c.getTime()));
+                viewArray.add(0);
+                invokeArray.add(0);
+                c.add(Calendar.DATE,1);
+            }
+
+        }else{
+            DailyViewCount dailyViewCount=dailyViewCountList.get(0);
+            Date firstDate = dailyViewCount.getDate();
+            c.add(Calendar.MONTH,-1);
+
+            if(dailyViewCountList.get(dailyViewCountList.size()-1).getDate().before(c.getTime())){
+                c.setTime(now);
+                c.add(Calendar.DATE,-6);
+            }
+
+            int index=0;
+            if(c.getTime().before(firstDate)){
+                c.setTime(firstDate);
+            }
+            else{
+                while(index<dailyViewCountList.size()&&c.getTime().after(dailyViewCountList.get(index).getDate())){
+                    index++;
+                }
+            }
+
+            startTime=sdf.format(c.getTime());
+
+            Calendar nowCalendar = Calendar.getInstance();
+            nowCalendar.setTime(now);
+            nowCalendar.add(Calendar.DATE, 1);
+
+            while (!Utils.isSameDay(c.getTime(),nowCalendar.getTime())){
+                dateList.add(sdf.format(c.getTime()));
+                if(index<dailyViewCountList.size()) {
+                    DailyViewCount daily = dailyViewCountList.get(index);
+
+                    if (Utils.isSameDay(daily.getDate(), c.getTime())) {
+                        int count=daily.getCount();
+                        if(count>max){
+                            max=count;
+                        }
+                        viewArray.add(count);
+                        index++;
+                    } else {
+                        viewArray.add(0);
+                    }
+                }
+                else{
+                    viewArray.add(0);
+                }
+
+                c.add(Calendar.DATE,1);
+                invokeArray.add(0);
+            }
+
+        }
+
+
+        for (int i = 0; i < computableModelList.size(); i++) {
+
+            setInvokeCount(computableModelList.get(i), startTime, invokeArray);
+
+        }
+
+        resultList.add(dateList);
+        resultList.add(viewArray);
+        resultList.add(invokeArray);
+
+        JSONObject result=new JSONObject();
+
+        result.put("valueList",resultList);
+
+        return result;
+    }
+
+    public void setInvokeCount(ComputableModel computableModel, String startTime, JSONArray invokeArray){
+        Calendar calendar=Calendar.getInstance();
+        try {
+            Date date=sdf.parse(startTime);
+            calendar.setTime(date);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+//        ComputableModel computableModel = computableModelList.get(i);
+        List<DailyViewCount> dailyInvokeCounts = computableModel.getDailyInvokeCount();
+
+        int index=0;
+        while (index<dailyInvokeCounts.size()&&calendar.getTime().after(dailyInvokeCounts.get(index).getDate())){
+            index++;
+        }
+
+        Calendar nowCalendar = Calendar.getInstance();
+        nowCalendar.setTime(new Date());
+        nowCalendar.add(Calendar.DATE, 1);
+
+        int count=1;
+        while (!Utils.isSameDay(calendar.getTime(),nowCalendar.getTime())){
+            if(index<dailyInvokeCounts.size()) {
+                DailyViewCount dailyInvokeCount = dailyInvokeCounts.get(index);
+                if (Utils.isSameDay(calendar.getTime(), dailyInvokeCount.getDate())) {
+                    int times=invokeArray.getInteger(count);
+                    times+=dailyInvokeCount.getCount();
+                    invokeArray.set(count,times);
+                    index++;
+                }
+            }
+
+            calendar.add(Calendar.DATE,1);
+            count++;
+        }
+    }
 
     public Integer[] viewTimesByHour(List<ViewRecord> viewRecordList) {
         Integer[] integers = new Integer[24];
