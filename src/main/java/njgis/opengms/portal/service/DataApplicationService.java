94270d91034b336e89809d7a4dd3992436200d5d
limingyuan123 <1484156313@qq.com>
2021-01-10 15:27:44 +0800

    1.10

--- src/main/java/njgis/opengms/portal/service/DataApplicationService.java
+++ src/main/java/njgis/opengms/portal/service/DataApplicationService.java
@@ -336,10 +336,9 @@ public class DataApplicationService {
                 invokeService.setServiceId(UUID.randomUUID().toString());//
                 invokeService.setMethod(dataApplication.getMethod());
                 invokeService.setName(dataApplication.getName());
-                //75
-                invokeService.setToken("fdtwTxlnhka8jY66lOT+kKutgZHnvi4NlnDc7QY5jR4=");
-                //33
-//                invokeService.setToken("fcky/35Rezr+Kyazr8SRWA==");
+
+//                invokeService.setToken("fdtwTxlnhka8jY66lOT+kKutgZHnvi4NlnDc7QY5jR4=");//75
+                invokeService.setToken("fcky/35Rezr+Kyazr8SRWA==");//33
                 invokeService.setContributor("Portal");
                 invokeService.setIsPortal(true);
                 List<InvokeService> invokeServices = new ArrayList<>();
@@ -697,10 +696,9 @@ public class DataApplicationService {
         Map<String, Object> part = new HashMap<>();
         part.put("uid", "0");//存在根目录中
         part.put("instype", "Data");
-        //33
-//        part.put("userToken", "f30f0e82-f6f1-4264-a302-caff7c40ccc9");
-        //75
-        part.put("userToken", "e3cea591-a8a5-4f50-b640-a569eccd94b7");
+
+        part.put("userToken", "f30f0e82-f6f1-4264-a302-caff7c40ccc9");//33
+//        part.put("userToken", "e3cea591-a8a5-4f50-b640-a569eccd94b7");//75
         String newFileId = UUID.randomUUID().toString();
         part.put("id", newFileId);
         part.put("oid", "0");
@@ -746,10 +744,9 @@ public class DataApplicationService {
         }
         part2.add("name", dataApplication.getName());
         //todo
-        //33
-//        part2.add("oid", "I3MXbzRq/NZkbWcKO8tF0w==");
-        //75
-        part2.add("oid", "5KglgbsDPmrFnA3J9CALzQ==");
+
+        part2.add("oid", "I3MXbzRq/NZkbWcKO8tF0w==");//33
+//        part2.add("oid", "5KglgbsDPmrFnA3J9CALzQ==");//75
 
         //获取xml
         String packageZipPath = resourcePath + "/DataApplication/Package" + dataApplication.getResources().get(0);
@@ -796,10 +793,9 @@ public class DataApplicationService {
         part2.add("relatedData", newFileId);//dataId
         part2.add("type", "Processing");
         part2.add("uid", "0");
-        //33
-//        part2.add("userToken", "f30f0e82-f6f1-4264-a302-caff7c40ccc9");
-        //75
-        part2.add("userToken", "e3cea591-a8a5-4f50-b640-a569eccd94b7");
+
+        part2.add("userToken", "f30f0e82-f6f1-4264-a302-caff7c40ccc9");//33
+//        part2.add("userToken", "e3cea591-a8a5-4f50-b640-a569eccd94b7");//75
         part2.add("processingPath", destDirPath);
 
         invokeService.setServiceId(serviceId);
