19023b7df87a66b0c3d2612987de1cbc7ef56a9a
kai <921485453@qq.com>
2021-01-11 16:44:34 +0800

    support初版内容填充

--- src/main/resources/static/js/page/support.js
+++ src/main/resources/static/js/page/support.js
@@ -12,7 +12,17 @@ var vue = new Vue({
         },
         methods:{
             showDoc(index){
-                window.location.href='/help/support/modelitem';
+                let obj={
+                    1:'Introduce to OpenGMS',
+                    2:'Model Resource',
+                    3:'Data Resource',
+                    4:'Service and Reuse',
+                    5:'Server Nodes',
+                    6:'Community',
+                    7:'Thematic Center',
+                    8:'User'
+                }
+                window.location.href='/help/support/'+obj[index].replaceAll(" ","_");
                 // this.docIndex=index;
                 // this.docLoading=true
                 // $('html,body').animate({scrollTop: '130px'}, 200);
